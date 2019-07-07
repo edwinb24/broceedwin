49292ea3515ecb708106a9b339605dc0faa8914b
edwinb24 <edwin_b24@hotmail.com>
2019-07-07 09:30:17 -0500

    Setup front-end to connect to cms-brocedesigns

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,18 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+        // Setup WPGraphQL.com to be the source
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        // This type will contain remote schema Query type
+        typeName: `WPGraphQL`,
+        // This is field under which it's accessible
+        fieldName: `wpgraphql`,
+        // Url to query from
+        url: `http://local.cms-brocedesigns.com/graphql?`,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
